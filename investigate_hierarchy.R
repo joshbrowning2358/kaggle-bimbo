cbf7879309032e19ebbd82188996f080cf7ee0a8
Joshua Browning <j.browning@coolblue.nl>
2016-07-01 15:46:50 +0200

    Push home

--- investigate_hierarchy.R
+++ investigate_hierarchy.R
@@ -2,19 +2,29 @@ library(data.table)
 library(xgboost)
 library(Matrix)
 
-sapply(dir("~/Personal Files/Kaggle/Model Framework/Cross-Validation/crossValidation/R/", full.names = TRUE),
+sapply(dir("../Model Framework/Cross-Validation/crossValidation/R/", full.names = TRUE),
        source)
 
-cnames = fread("data/train.csv", nrow=0)
-train = fread("data/train_sample_75k.csv", col.names=colnames(cnames))
+# cnames = fread("data/train.csv", nrow=0)
+# train = fread("data/train_sample_75k.csv", col.names=colnames(cnames))
+train = fread("train_complete_sample_45k.csv")
 
 predModel = function(model, X){
-    X = merge(X, model, by=c("Cliente_ID", "Semana"), all.x=TRUE)
+    X = merge(X, model[[1]], by=c("Cliente_ID", "Semana"), all.x=TRUE)
+    X = merge(X, model[[2]], by=c("Ruta_SAK", "Semana"), all.x=TRUE)
+    X = merge(X, model[[3]], by=c("Agencia_ID", "Semana"), all.x=TRUE)
+    X = merge(X, model[[4]], by=c("Canal_ID", "Semana"), all.x=TRUE)
+    X[is.na(groupMean), groupMean := groupMean2]
+    X[is.na(groupMean), groupMean := groupMean3]
+    X[is.na(groupMean), groupMean := groupMean4]
     X$groupMean
 }
 fitModel = function(X, y){
     X[, y := y]
-    X[, list(groupMean = mean(y)), by=c("Cliente_ID", "Semana")]
+    list(X[, list(groupMean = mean(y)), by=c("Cliente_ID", "Semana")],
+         X[, list(groupMean2 = mean(y)), by=c("Ruta_SAK", "Semana")],
+         X[, list(groupMean3 = mean(y)), by=c("Agencia_ID", "Semana")],
+         X[, list(groupMean4 = mean(y)), by=c("Canal_ID", "Semana")])
 }
 productoAvg = crossValidation(model=list(predict=predModel, fit=fitModel),
                               xTrain = train,
@@ -22,4 +32,4 @@ productoAvg = crossValidation(model=list(predict=predModel, fit=fitModel),
                               xTest = train[1, ],
                               cvIndices = train$Producto_ID)
 summary(productoAvg)
-run(productoAvg)
+run(productoAvg, metric=RMSLE)
---
+++ subsample_by_feature.R
@@ -0,0 +1,7 @@
+set.seed(123)
+train = fread("data/train.csv")
+sample_prods = sample(unique(train$Producto_ID), size=1000)
+sample_cliente = sample(unique(train$Cliente_ID), size=1000)
+
+write.csv(train[Cliente_ID %in% sample_cliente & Producto_ID %in% sample_prods, ],
+          file="data/train_complete_sample_45k.csv", row.names=FALSE)
